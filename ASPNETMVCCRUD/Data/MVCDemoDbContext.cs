3aa2348bcdc44fc082e0f926d21dca41b457f45d
Gor <Gor@DESKTOP-Q53V3V6>
2023-12-31 19:24:40 +0400

    Add project files.

---
+++ ASPNETMVCCRUD/Data/MVCDemoDbContext.cs
@@ -0,0 +1,17 @@
+﻿using ASPNETMVCCRUD.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPNETMVCCRUD.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        //private DbSet<Employee> myProperty;
+
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get ; set; }
+    }
+}
